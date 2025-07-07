27bdc7a47271c3f3805d8801de01616ba53ca1f0
Твоё Имя <@email.com>
2025-07-08 01:45:13 +0300

    Исправлен импорт ChangePasswordView на PasswordChangeView в users.urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,7 +5,7 @@ from .views import (
     UserLoginView,
     UserLogoutView,
     UserProfileView,
-    ChangePasswordView,
+    PasswordChangeView,
     EmailVerificationView,
     ResetPasswordView,
     ResetPasswordConfirmView,
